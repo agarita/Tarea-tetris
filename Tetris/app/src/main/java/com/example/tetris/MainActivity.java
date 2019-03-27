0cbf6c6683de100b2bf413c2f5cda1b30b74d2ee
Alejandro Garita <luisgc2009@gmail.com>
2019-03-27 03:49:26 -0600

    Arreglos interfaz
    la interfaz se deja bonita y presentable

--- Tetris/app/src/main/java/com/example/tetris/MainActivity.java
+++ Tetris/app/src/main/java/com/example/tetris/MainActivity.java
@@ -1,41 +1,202 @@
 package com.example.tetris;
 
-import android.media.Image;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayout;
 import android.util.Log;
-import android.widget.GridView;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+    /*
+    * Datos de matriz:
+    *   0 = nada
+    *   1 = L {(x,y),(x+1,y),(x+2,y),(x+2,y-1)}
+    *   2 = J {(x,y),(x+1,y),(x+2,y),(x+2,y+1)}
+    *   3 = S {(x,y),(x+1,y),(x+1,y-1),(x+2,y-1)}
+    *   4 = Z {(x,y),(x+1,y),(x+1,y+1),(x+2,y+1)}
+    *   5 = T {(x,y),(x+1,y-1),(x+1,y),(x+1,y+1)}
+    *   6 = I {(x,y),(x+1,y),(x+2,y),(x+3,y)}
+    *   7 = O {(x,y),(x,y+1),(x+1,y),(x+1,y+1)}
+    * */
 
+    ArrayList<ArrayList<Integer>> matriz;
+    GridLayout grid;
+    ArrayList<ArrayList<Integer>> fichaActual;
+    int tipoFichaActual;
 
+    void pintarTablero(){
+        int child = 0;
+        for(int i=0; i<11; i++){
+            for(int j=0; j<11; j++){
+                ImageView view = (ImageView) grid.getChildAt(child);
+                if(i==0 || i==10 || j==0 || j==10){
+                    view.setImageResource(android.R.color.darker_gray);
+                }
+                else{
+                    switch (matriz.get(i).get(j)){
+                        case 1: view.setImageResource(android.R.color.holo_orange_dark);break;
+                        case 2: view.setImageResource(android.R.color.holo_blue_dark);break;
+                        case 3: view.setImageResource(android.R.color.holo_green_dark);break;
+                        case 4: view.setImageResource(android.R.color.holo_red_dark);break;
+                        case 5: view.setImageResource(android.R.color.holo_purple);break;
+                        case 6: view.setImageResource(android.R.color.holo_blue_bright);break;
+                        case 7: view.setImageResource(R.color.amarillo);break;
+                        default: view.setImageResource(R.color.fondo);break;
+                    }
+                }
+                child++;
+            }
+        }
+    }
+
+    void bajarFicha(){
+        int fil,col;
+        for(int i=0; i<4; i++){
+            fil = fichaActual.get(i).get(1);
+            col = fichaActual.get(i).get(2);
+            matriz.get(fil).set(col,0);
+            int valor = fichaActual.get(i).get(1);
+            fichaActual.get(i).set(1, valor+1);
+            fil = fichaActual.get(i).get(1);
+            col = fichaActual.get(i).get(2);
+            matriz.get(fil).set(col,tipoFichaActual);
+        }
+    }
+
+    void setFichaActual(int modo, int fil, int col){
+        ArrayList<Integer> pos;
+        pos = new ArrayList<>();
+        fichaActual.clear();
+        switch (modo){
+            case 1://L
+                pos.clear();pos.add(fil); pos.add(col);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+1); pos.add(col);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+2); pos.add(col);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+2); pos.add(col+1);
+                fichaActual.add(pos);
+                break;
+            case 2://J
+                pos.clear();pos.add(fil); pos.add(col);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+1); pos.add(col);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+2); pos.add(col);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+2); pos.add(col-1);
+                fichaActual.add(pos);
+                break;
+            case 3: //S
+                pos.clear();pos.add(fil); pos.add(col);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+1); pos.add(col);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+1); pos.add(col+1);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+2); pos.add(col+1);
+                fichaActual.add(pos);
+                break;
+            case 4: //Z
+                pos.clear();pos.add(fil); pos.add(col);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+1); pos.add(col);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+1); pos.add(col-1);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+2); pos.add(col-1);
+                fichaActual.add(pos);
+                break;
+            case 5: //T
+                pos.clear();pos.add(fil); pos.add(col);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+1); pos.add(col-1);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+1); pos.add(col);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+1); pos.add(col+1);
+                fichaActual.add(pos);
+                break;
+            case 6: //I
+                pos.clear();pos.add(fil); pos.add(col);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+1); pos.add(col);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+2); pos.add(col);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+3); pos.add(col);
+                fichaActual.add(pos);
+                break;
+            case 7: // O
+                pos.clear();pos.add(fil); pos.add(col);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil); pos.add(col+1);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+1); pos.add(col);
+                fichaActual.add(pos);
+                pos.clear();pos.add(fil+1); pos.add(col+1);
+                fichaActual.add(pos);
+                break;
+            default:
+                break;
+        }
+    }
+
+    void inicializarMatriz(){
+        matriz = new ArrayList<>();
+        ArrayList<Integer> col = new ArrayList<>();
+        for(int i=0; i<10; i++){
+            col.clear();
+            for(int j=0; j<10; j++){
+                col.add(0);
+            }
+            matriz.add(col);
+        }
+    }
+
+    void leerMatriz(){
+        for(int i=0; i<10; i++){
+            Log.i("Info", String.format("%s", matriz.get(i).toString()));
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        GridLayout grid = (GridLayout) findViewById(R.id.gridLayout2);
 
-        grid.setColumnCount(10);
-        grid.setRowCount(10);
+        grid = findViewById(R.id.gridLayout);
+        inicializarMatriz();
 
         int colCount = grid.getColumnCount();
         int rowCount = grid.getRowCount();
         for (int i=0; i < colCount*rowCount; i++){
-            grid.addView(new ImageView(this));
+            ImageView cuadro = new ImageView(this);
+            cuadro.setId(i);
+            ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            cuadro.setLayoutParams(params);
+            ViewGroup.MarginLayoutParams marginParams = new ViewGroup.MarginLayoutParams(cuadro.getLayoutParams());
+            marginParams.setMargins(2, 2, 2, 2);
+            cuadro.setLayoutParams(marginParams);
+            cuadro.requestLayout();
+            cuadro.getLayoutParams().height = (grid.getLayoutParams().height/11)-3;
+            cuadro.getLayoutParams().width = (grid.getLayoutParams().width/11)-3;
+            grid.addView(cuadro);
         }
 
-        int count = grid.getChildCount();
+        pintarTablero();
 
-        for (int i=0; i < count; i++){
-            ImageView view = (ImageView) grid.getChildAt(i);
-            view.setImageResource(R.drawable.cuadro);
-        }
-
-        Log.i("Info", String.valueOf(count));
-
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                //pintarTablero();
+            }
+        }, 0, 1000);
     }
 }
